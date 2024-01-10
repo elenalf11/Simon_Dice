34ccce417ea1e6ba4a6767d8a6471d35e3bcad02
elenalf11 <elena.lopezfelix@colegiolitterator.com>
2024-01-10 12:58:38 +0100

    clase Jugador acabada

--- Simon_Dice/src/main/Engine.java
+++ Simon_Dice/src/main/Engine.java
@@ -1,5 +1,8 @@
 package main;
+import java.util.Scanner;
 
 public class Engine {
+	Scanner scanner = new Scanner(System.in);
+	
 
 }
--- Simon_Dice/src/main/Jugador.java
+++ Simon_Dice/src/main/Jugador.java
@@ -1,11 +1,32 @@
 package main;
 
 public class Jugador {
+	/**
+	 * Atributos
+	 */
 	private String nombre;
 	private int puntuacion;
-	
+	/**
+	 * Constructora 
+	 * @param nombre
+	 */
 	public Jugador(String nombre) {
 		this.nombre = nombre;
+	}
+	/**
+	 * Métodos
+	 */
+	public String getNombre() {
+		return this.nombre;
+	}
+	public int getPuntuacion() {
+		return this.puntuacion;
+	}
+	public void setNombre() {
+		this.nombre = nombre;
+	}
+	public void setPuntuacion() {
+		this.puntuacion = puntuacion;
 	}
 
 }
