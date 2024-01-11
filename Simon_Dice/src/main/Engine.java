68a8e6e72c574a2660804de93e2af8e6271c0f95
elenalf11 <elena.lopezfelix@colegiolitterator.com>
2024-01-11 18:52:58 +0100

    metodo intToColor en Engine

--- Simon_Dice/src/main/Engine.java
+++ Simon_Dice/src/main/Engine.java
@@ -28,34 +28,59 @@ public class Engine {
 	
 	public tColores charToColor(char _color) {
 		tColores elegir = null;
-			switch (_color) {
-			case 'a':
-				elegir = tColores.AZUL;
-				break;
-			case 'A':
-				elegir = tColores.AZUL;
-				break;
-			case 'r':
-				elegir = tColores.ROJO;
-				break;
-			case 'R':
-				elegir = tColores.ROJO;
-				break;
-			case 'v':
-				elegir = tColores.VERDE;
-				break;
-			case 'V':
-				elegir = tColores.VERDE;
-				break;
-			case 'd':
-				elegir = tColores.DORADO;
-				break;
-			case 'D':
-				elegir = tColores.DORADO;
-				break;
-			default:
-				System.out.println("Caracter incorrecto, no se corresponde con ningún color.");	
-			}
+		switch (_color) {
+		case 'a':
+			elegir = tColores.AZUL;
+			break;
+		case 'A':
+			elegir = tColores.AZUL;
+			break;
+		case 'r':
+			elegir = tColores.ROJO;
+			break;
+		case 'R':
+			elegir = tColores.ROJO;
+			break;
+		case 'v':
+			elegir = tColores.VERDE;
+			break;
+		case 'V':
+			elegir = tColores.VERDE;
+			break;
+		case 'd':
+			elegir = tColores.DORADO;
+			break;
+		case 'D':
+			elegir = tColores.DORADO;
+			break;
+		default:
+			System.out.println("Caracter incorrecto, no se corresponde con ningún color.");	
+		}
+		return elegir;
+	}
+	/**
+	 * Método que relaciona un número(posición del enum tColores) con un color incluido en el enum de tColores
+	 * @param _numero
+	 * @return
+	 */
+	public tColores intToColor(int _numero) {
+		tColores elegir = null;
+		switch(_numero) {
+		case 0:
+			elegir = tColores.ROJO;
+			break;
+		case 1:
+			elegir = tColores.VERDE;
+			break;
+		case 2:
+			elegir = tColores.AZUL;
+			break;
+		case 3:
+			elegir = tColores.DORADO;
+			break;
+		default:
+			System.out.println("Número incorrecto, no se corresponde con ningún color.");
+		}
 		return elegir;
 	}
 	
